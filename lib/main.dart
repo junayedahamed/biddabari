aa0d32df2e758af2c9d97bdfca1f6d67f5ebb180
junayedahamed <junayedahamed660@gmail.com>
2026-09-10 18:16:49 +0600

    course cached

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,14 @@
+import 'package:biddabari/course/logic/network_service.dart';
 import 'package:biddabari/course/bindings/course_bindings.dart';
 import 'package:biddabari/router/app_routes.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:get_storage/get_storage.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await GetStorage.init();
+  Get.put(NetworkService(), permanent: true);
   runApp(const MyApp());
 }
 
