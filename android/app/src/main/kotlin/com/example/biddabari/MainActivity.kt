94a47c1d6c4621150c31de8e77c3c78a8cd3fa95
junayedahamed <junayedahamed660@gmail.com>
2026-09-10 16:07:52 +0600

    initial of project

---
+++ android/app/src/main/kotlin/com/example/biddabari/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.biddabari
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/course/data/repository/course_repository.dart
@@ -0,0 +1,3 @@
+class CourseRepository {
+  final String baseUrl = "https://api.biddabari.com/api/v1/app-home-courses";
+}
